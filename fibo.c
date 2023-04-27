330f4ccb6eb4456029388add17fba8802a465c18
Ouaouicha Anas <btss22aouaou@eleve-irup.com>
2023-04-27 11:21:21 +0200

    premier commit

---
+++ fibo.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int a = 0;
+    int b = 1;
+    int c = 0;
+
+    printf("%d\n%d\n", a, b);
+
+    while ( c < 100) {
+        c  = a + b;
+        a = b;
+        b = c;
+        printf("%d\n", c);
+    }
+
+}
